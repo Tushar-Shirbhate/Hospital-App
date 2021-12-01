0763e1b89b83a141e82e1766edb5d0a6a541c3e2
rik <rikchat15@gmail.com>
2021-12-01 19:02:13 +0530

    random change

--- lib/pages/edit_profilePage.dart
+++ lib/pages/edit_profilePage.dart
@@ -5,6 +5,7 @@ import 'package:hospital_app/utils/user.dart';
 import 'package:hospital_app/widgets/profile_widget.dart';
 import 'package:hospital_app/widgets/text_field_widget.dart';
 import 'package:hospital_app/widgets/user_preferences.dart';
+import 'package:hospital_app/utils/routes.dart';
 
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
@@ -54,6 +55,24 @@ class _EditProfilePageState extends State<EditProfilePage> {
             text: user.address,
             maxLines: 5,
             onChanged: (address) {},
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, MyRoute.profileRoute);
+                  },
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ))
+            ],
           )
         ],
       ),
